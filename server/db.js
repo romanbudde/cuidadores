133dd7ed9146ae12fb69aa5c3ba4148328e4de7f
Roman Budde <roman_budde@hotmail.com>
2023-03-16 19:47:33 -0300

    added dockerization. Functional

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: "postgres",
     password: "123456",
-    host: "localhost",
+    host: "db",
     port: "5432",
     database: "db_cuidadores"
 });

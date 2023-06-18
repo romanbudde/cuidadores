5f62e99b2090bf428bceaaa0ae4d360319df3e5c
Roman Budde <roman_budde@hotmail.com>
2023-06-18 19:46:14 -0300

    added package for address validation API. To do: put the api key and make it work :D.

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import Cookies from 'universal-cookie';
 import { AuthProvider, AuthContext } from './AuthContext';
+import { Autocomplete } from '@lob/react-address-autocomplete'
 
 const Register = () => {
 
@@ -75,69 +76,74 @@ const Register = () => {
 
   return (
     <Fragment>
-      <form className="space-y-5 p-10 my-20 mx-auto flex flex-col justify-center items-center bg-gray-100 min-w-70 w-96 rounded-md bg-slate-200">
-        <h4 className="text-center">Cuidar</h4>
-        <h4></h4>
-        <div className='flex flex-col w-full'>
-          <label className="block mb-2 mr-auto text-sm font-medium text-gray-900 dark:text-white">
-              Email
-          </label>
-          <input
-              type="email"
-              name="email"
-              className="bg-gray-50 border text-gray-900 text-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white  bg-transparent rounded-lg border-b border-gray-400 border-solid border-opacity-100 focus:outline-none focus:outline-0"
-              placeholder="youremail@email.com"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-          />
+      <form className="min-w-70 w-96 rounded-md">
+        <div className='flex flex-row items-center w-full justify-center relative border-b-2 border-b-gray-200'>
+          <h1 className='flex justify-center font-bold text-lg py-4'>Cuidar</h1>
         </div>
-        <div className='flex flex-col w-full'>
-          <label className="block mb-2 mr-auto text-sm font-medium text-gray-900 dark:text-white">
-                Password
-          </label>
-          <input
-              type="password"
-              name="password"
-              className="bg-gray-50 border text-gray-900 text-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white  bg-transparent rounded-lg border-b border-gray-400 border-solid border-opacity-100 focus:outline-none focus:outline-0"
-              value={password}
-              placeholder="•••••••••"
-              onChange={e => setPassword(e.target.value)}
-          />
-        </div>
-        <div className='flex flex-col w-full'>
-          <label className="block mb-2 mr-auto text-sm font-medium text-gray-900 dark:text-white">
-              First Name
-          </label>
-          <input
-              type="text"
-              name="firstname"
-              className="bg-gray-50 border text-gray-900 text-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white  bg-transparent rounded-lg border-b border-gray-400 border-solid border-opacity-100 focus:outline-none focus:outline-0"
-              placeholder="ej: Pedro"
-              value={firstname}
-              onChange={e => setFirstname(e.target.value)}
-          />
-        </div>
-        <div className='flex flex-col w-full'>
+        <div className='space-y-2 px-10 py-5 mx-auto flex flex-col justify-center items-center'>
+          <h4 className='w-full text-center font-semibold mb-3 text-gray-500'>Crea tu cuenta</h4>
+          <div className='flex flex-col w-full'>
             <label className="block mb-2 mr-auto text-sm font-medium text-gray-900 dark:text-white">
-                Last Name
+                Email
+            </label>
+            <input
+                type="email"
+                name="email"
+                className="bg-gray-50 border text-gray-900 text-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white  bg-transparent rounded-lg border-b border-gray-400 border-solid border-opacity-100 focus:outline-none focus:outline-0"
+                placeholder="youremail@email.com"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='flex flex-col w-full'>
+            <label className="block mb-2 mr-auto text-sm font-medium text-gray-900 dark:text-white">
+                  Password
+            </label>
+            <input
+                type="password"
+                name="password"
+                className="bg-gray-50 border text-gray-900 text-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white  bg-transparent rounded-lg border-b border-gray-400 border-solid border-opacity-100 focus:outline-none focus:outline-0"
+                value={password}
+                placeholder="•••••••••"
+                onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <div className='flex flex-col w-full'>
+            <label className="block mb-2 mr-auto text-sm font-medium text-gray-900 dark:text-white">
+                First Name
             </label>
             <input
                 type="text"
-                name="lastname"
+                name="firstname"
                 className="bg-gray-50 border text-gray-900 text-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white  bg-transparent rounded-lg border-b border-gray-400 border-solid border-opacity-100 focus:outline-none focus:outline-0"
-                value={lastname}
-                placeholder="ej: Gomez"
-                onChange={e => setLastname(e.target.value)}
-                required
+                placeholder="ej: Pedro"
+                value={firstname}
+                onChange={e => setFirstname(e.target.value)}
             />
+          </div>
+          <div className='flex flex-col w-full'>
+              <label className="block mb-2 mr-auto text-sm font-medium text-gray-900 dark:text-white">
+                  Last Name
+              </label>
+              <input
+                  type="text"
+                  name="lastname"
+                  className="bg-gray-50 border text-gray-900 text-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white  bg-transparent rounded-lg border-b border-gray-400 border-solid border-opacity-100 focus:outline-none focus:outline-0"
+                  value={lastname}
+                  placeholder="ej: Gomez"
+                  onChange={e => setLastname(e.target.value)}
+                  required
+              />
+          </div>
+          <Autocomplete apiKey="test_pub_9fd4d84a5e6a3603f999ef25a14e6a5" />
+          <button 
+              type="submit"
+              className="w-full text-white bg-gradient-to-r from-green-400 to-green-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={ (e) => { onSubmitUser(e); }}
+          >
+            Crear cuenta
+          </button>
         </div>
-        <button 
-            type="submit"
-            className="w-full text-white bg-gradient-to-r from-green-400 to-green-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={ (e) => { onSubmitUser(e); }}
-        >
-          Crear cuenta
-        </button>
       </form>
 
     </Fragment>

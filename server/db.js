c5dbef2a387d9b0c11378e4be32aeccfb1cf52bd
Roman Budde <roman_budde@hotmail.com>
2023-07-29 11:56:34 -0300

    database name at db.js so that it points to prod

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     password: "UhsUFndhYvBu8Op8r3qB4z2U3sqnA1OY",
     host: "dpg-cj2hk5l9aq0e0q25ebp0-a.oregon-postgres.render.com",
     port: "5432",
-    database: "db_cuidadores_9a2e",
+    database: "db_cuidadores",
     ssl: true
 });
 
